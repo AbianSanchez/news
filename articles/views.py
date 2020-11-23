59afbcf7737940e38b3dedd17d8a96d8abe1520e
Abian Sanchez <AbianSanchezM@gmail.com>
2020-11-23 23:30:38 +0000

    Permissions and Authorization

--- articles/views.py
+++ articles/views.py
@@ -1,29 +1,48 @@
 #from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login' #when a not authorized user access to articles, be redirec to login view.
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+    def test_func(self): # Check if the current user is the author of the current article.
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
-class ArticleCreateView(CreateView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body',)
+    login_url = 'login'  #Tell to LoginRequiredMixin to override login to user/login
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
