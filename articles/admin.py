7feae53c4fb41656795898d3969a1c898a7a5c20
Abian Sanchez <AbianSanchezM@gmail.com>
2020-11-24 17:51:51 +0000

    Add Article comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+'''class CommentInline(admin.StackedInline): # List of comments
+    model = Comment
+    extra = 0  # Number of empty comments that appears after the comments'''
+
+class CommentInline(admin.TabularInline): # List of comments simplified
+    model = Comment
+    extra = 0  # Number of empty comments that appears after the comments
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
